c03564e337dcfc641d0c5a697d49488f1a1687e5
Brendan Jackman <jackmanb@google.com>
2023-07-28 14:23:10 +0000

    mm: asi: ASI support in interrupts/exceptions
    Add support for potentially switching address spaces from within
    interrupts/exceptions/NMIs etc. An interrupt does not automatically
    switch to the sensitive address space. It can switch if needed to
    access some memory not available in the nonsensitive address space,
    using the normal asi_exit call.
    
    On return from the outermost interrupt, if the target address space was
    the nonsensitive address space (e.g. we were in the critical code path
    between ASI Enter and VM Enter), the nonsensitive address space will be
    automatically restored. Otherwise, execution will continue in the
    sensitive address space until the next explicit ASI Enter.
    
    In order to keep track of when to restore the nonsensitive address
    space, an interrupt/exception nesting depth counter is maintained
    per-task. An alternative implementation without needing this counter is
    also possible, but the counter unlocks an additional nice-to-have
    benefit by allowing detection of whether or not we are currently
    executing inside an exception context, which would be useful in a later
    patch.
    
    Note that for KVM on SVM, this is not actually necessary as NMIs are in
    fact maskable via CLGI. It's not clear to me if VMX has something
    equivalent but we will need this infrastructure in place for userspace
    support anyway.
    
    RFC: Once userspace ASI is implemented, this idtentry integration
    looks a bit heavy-handed. For example, we don't need this logic
    for INT 80 emulation, so having it in DEFINE_IDTENTRY_RAW is
    confusing. It could lead to a bug if the order of interrupter counter
    modifications and ASI transition logic gets flipped around somehow.
    
    TODO: Update API per https://lore.kernel.org/linux-mm/20250219105503.GKZ7W4h6QW1CNj48U9@fat_crate.local/
    
    Signed-off-by: Junaid Shahid <junaids@google.com>
    Signed-off-by: Brendan Jackman <jackmanb@google.com>
    Change-Id: I718d3c55f9f556c442c1d11f7d86fcce93a9c39b

--- arch/x86/include/asm/asi.h
+++ arch/x86/include/asm/asi.h
@@ -2,6 +2,8 @@
 #ifndef _ASM_X86_ASI_H
 #define _ASM_X86_ASI_H
 
+#include <linux/asi.h>
+#include <linux/log2.h>
 #include <linux/sched.h>
 
 #include <asm/cpufeature.h>
@@ -157,6 +159,11 @@ void asi_relax(void);
 /* Immediately exit the nonsensitive address space if in it */
 void asi_exit(void);
 
+static inline void asi_init_thread_state(struct thread_struct *thread)
+{
+	thread->asi_state.intr_nest_depth = 0;
+}
+
 /* The target is the domain we'll enter when returning to process context. */
 static __always_inline struct asi *asi_get_target(struct task_struct *p)
 {
@@ -167,6 +174,94 @@ static __always_inline void asi_set_target(struct task_struct *p,
 					   struct asi *target)
 {
 	p->thread.asi_state.target = target;
+}
+
+static __always_inline struct asi *asi_get_current(void)
+{
+	return asi_enabled_static()
+	       ? this_cpu_read(curr_asi)
+	       : NULL;
+}
+
+/* Are we currently in a nonsensitive address space? */
+static __always_inline bool asi_in_nonsensitive(void)
+{
+	return (bool)asi_get_current();
+}
+
+/* If we exit/have exited, can we stay that way until the next asi_enter? */
+static __always_inline bool asi_is_relaxed(void)
+{
+	return !asi_get_target(current);
+}
+
+/*
+ * Is the current task in the critical section?
+ *
+ * This is just the inverse of !asi_is_relaxed(). We have both functions in
+ * order to help write intuitive client code. In particular, asi_is_tense
+ * returns false when ASI is disabled, which is judged to make user code more
+ * obvious.
+ */
+static __always_inline bool asi_is_tense(void)
+{
+	return !asi_is_relaxed();
+}
+
+static __always_inline void asi_intr_enter(void)
+{
+	if (asi_enabled_static() && asi_is_tense()) {
+		current->thread.asi_state.intr_nest_depth++;
+		barrier();
+	}
+}
+
+void __asi_enter(void);
+
+static __always_inline void asi_intr_exit(void)
+{
+	if (asi_enabled_static() && asi_is_tense()) {
+		/*
+		 * If an access to sensitive memory got reordered after the
+		 * decrement, the #PF handler for that access would see a value
+		 * of 0 for the counter and re-__asi_enter before returning to
+		 * the faulting access, triggering an infinite PF loop.
+		 */
+		barrier();
+
+		if (--current->thread.asi_state.intr_nest_depth == 0) {
+			/*
+			 * If the decrement got reordered after __asi_enter, an
+			 * interrupt that came between __asi_enter and the
+			 * decrement would always see a nonzero value for the
+			 * counter so it wouldn't call __asi_enter again and we
+			 * would return to process context in the wrong address
+			 * space.
+			 */
+			barrier();
+			__asi_enter();
+		}
+	}
+}
+
+/*
+ * Returns the nesting depth of interrupts/exceptions that have interrupted the
+ * ongoing critical section. If the current task is not in a critical section
+ * this is 0.
+ */
+static __always_inline int asi_intr_nest_depth(void)
+{
+	return current->thread.asi_state.intr_nest_depth;
+}
+
+/*
+ * Remember that interrupts/exception don't count as the critical section. If
+ * you want to know if the current task is in the critical section use
+ * asi_is_tense().
+ */
+static __always_inline bool asi_in_critical_section(void)
+{
+	return asi_is_tense() && !asi_intr_nest_depth();
 }
 
 #define INIT_MM_ASI(init_mm) \
