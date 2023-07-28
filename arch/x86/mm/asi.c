3b14b701e89adb7668b5370d5f1c53bc8202b395
Brendan Jackman <jackmanb@google.com>
2023-07-28 14:23:10 +0000

    mm: asi: ASI support in interrupts/exceptions
    Add support for potentially switching address spaces from within
    interrupts/exceptions/NMIs etc. An interrupt does not automatically
    switch to the sensitive address space. It can switch if needed to
    access some memory not available in the nonsensitive address space,
    using the normal asi_exit call.
    
    On return from the outermost interrupt, if the target address space was
    the nonsensitive address space (e.g. we were in the critical code path
    between ASI Enter and VM Enter), the nonsensitive address space will be
    automatically restored. Otherwise, execution will continue in the
    sensitive address space until the next explicit ASI Enter.
    
    In order to keep track of when to restore the nonsensitive address
    space, an interrupt/exception nesting depth counter is maintained
    per-task. An alternative implementation without needing this counter is
    also possible, but the counter unlocks an additional nice-to-have
    benefit by allowing detection of whether or not we are currently
    executing inside an exception context, which would be useful in a later
    patch.
    
    Note that for KVM on SVM, this is not actually necessary as NMIs are in
    fact maskable via CLGI. It's not clear to me if VMX has something
    equivalent but we will need this infrastructure in place for userspace
    support anyway.
    
    RFC: Once userspace ASI is implemented, this idtentry integration
    looks a bit heavy-handed. For example, we don't need this logic
    for INT 80 emulation, so having it in DEFINE_IDTENTRY_RAW is
    confusing. It could lead to a bug if the order of interrupter counter
    modifications and ASI transition logic gets flipped around somehow.
    
    TODO: Update API per https://lore.kernel.org/linux-mm/20250219105503.GKZ7W4h6QW1CNj48U9@fat_crate.local/
    
    Signed-off-by: Junaid Shahid <junaids@google.com>
    Signed-off-by: Brendan Jackman <jackmanb@google.com>
    Change-Id: I718d3c55f9f556c442c1d11f7d86fcce93a9c39b

--- arch/x86/mm/asi.c
+++ arch/x86/mm/asi.c
@@ -205,9 +205,10 @@ static __always_inline void maybe_flush_data(struct asi *next_asi)
 	this_cpu_and(asi_taints, ~ASI_TAINTS_DATA_MASK);
 }
 
-static noinstr void __asi_enter(void)
+noinstr void __asi_enter(void)
 {
 	u64 asi_cr3;
+	u16 pcid;
 	struct asi *target = asi_get_target(current);
 
 	/*
@@ -220,6 +221,7 @@ static noinstr void __asi_enter(void)
 	 * disabling preemption should be fine.
 	 */
 	VM_BUG_ON(preemptible());
+	VM_BUG_ON(current->thread.asi_state.intr_nest_depth != 0);
 
 	if (!target || target == this_cpu_read(curr_asi))
 		return;
@@ -235,9 +237,8 @@ static noinstr void __asi_enter(void)
 	this_cpu_write(curr_asi, target);
 	maybe_flush_control(target);
 
-	asi_cr3 = build_cr3_noinstr(target->pgd,
-				    this_cpu_read(cpu_tlbstate.loaded_mm_asid),
-				    tlbstate_lam_cr3_mask());
+	pcid = asi_pcid(target, this_cpu_read(cpu_tlbstate.loaded_mm_asid));
+	asi_cr3 = build_cr3_pcid_noinstr(target->pgd, pcid, tlbstate_lam_cr3_mask(), false);
 	write_cr3(asi_cr3);
 
 	maybe_flush_data(target);
@@ -257,6 +258,10 @@ static noinstr void __asi_enter(void)
 
 noinstr void asi_enter(struct asi *asi)
 {
+	if (!cpu_feature_enabled(X86_FEATURE_ASI))
+		return;
+
+	VM_WARN_ON_ONCE(asi_intr_nest_depth());
 	VM_WARN_ON_ONCE(!asi);
 
 	/* Should not have an asi_enter() without a prior asi_relax(). */
@@ -271,8 +276,11 @@ EXPORT_SYMBOL_GPL(asi_enter);
 
 noinstr void asi_relax(void)
 {
-	barrier();
-	asi_set_target(current, NULL);
+	if (cpu_feature_enabled(X86_FEATURE_ASI)) {
+		VM_WARN_ON_ONCE(asi_intr_nest_depth());
+		barrier();
+		asi_set_target(current, NULL);
+	}
 }
 EXPORT_SYMBOL_GPL(asi_relax);
 
@@ -288,12 +296,14 @@ noinstr void asi_exit(void)
 
 	asi = this_cpu_read(curr_asi);
 	if (asi) {
+		WARN_ON_ONCE(asi_in_critical_section());
+
 		maybe_flush_control(NULL);
 
 		sensitive_cr3 =
 			build_cr3_noinstr(this_cpu_read(cpu_tlbstate.loaded_mm)->pgd,
-					  this_cpu_read(cpu_tlbstate.loaded_mm_asid),
-					  tlbstate_lam_cr3_mask());
+					 this_cpu_read(cpu_tlbstate.loaded_mm_asid),
+					 tlbstate_lam_cr3_mask());
 
 		/* Tainting first makes reentrancy easier to reason about.  */
 		this_cpu_or(asi_taints, ASI_TAINT_KERNEL_DATA);
