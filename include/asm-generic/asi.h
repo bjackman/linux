508b6d41023aa7fcf563832dd29ad1dcbea43a1f
Brendan Jackman <jackmanb@google.com>
2023-07-28 14:23:10 +0000

    mm: asi: ASI support in interrupts/exceptions
    Add support for potentially switching address spaces from within
    interrupts/exceptions/NMIs etc. An interrupt does not automatically
    switch to the unrestricted address space. It can switch if needed to
    access some memory not available in the restricted address space, using
    the normal asi_exit call.
    
    On return from the outermost interrupt, if the target address space was
    the restricted address space (e.g. we were in the critical code path
    between ASI Enter and VM Enter), the restricted address space will be
    automatically restored. Otherwise, execution will continue in the
    unrestricted address space until the next explicit ASI Enter.
    
    In order to keep track of when to restore the restricted address space,
    an interrupt/exception nesting depth counter is maintained per-task.
    An alternative implementation without needing this counter is also
    possible, but the counter unlocks an additional nice-to-have benefit by
    allowing detection of whether or not we are currently executing inside
    an exception context, which would be useful in a later patch.
    
    Note that for KVM on SVM, this is not actually necessary as NMIs are in
    fact maskable via CLGI. It's not clear to me if VMX has something
    equivalent but we will need this infrastructure in place for userspace
    support anyway.
    
    RFC: Once userspace ASI is implemented, this idtentry integration
    looks a bit heavy-handed. For example, we don't need this logic
    for INT 80 emulation, so having it in DEFINE_IDTENTRY_RAW is
    confusing. It could lead to a bug if the order of interrupter counter
    modifications and ASI transition logic gets flipped around somehow.
    
    checkpatch.pl SPACING is false positive. AVOID_BUG ignored for RFC.
    
    Checkpatch-args: --ignore=SPACING,AVOID_BUG
    Signed-off-by: Junaid Shahid <junaids@google.com>
    Signed-off-by: Brendan Jackman <jackmanb@google.com>
    Change-Id: I718d3c55f9f556c442c1d11f7d86fcce93a9c39b

--- include/asm-generic/asi.h
+++ include/asm-generic/asi.h
@@ -52,6 +52,8 @@ static inline bool asi_is_relaxed(void) { return true; }
 
 static inline bool asi_is_tense(void) { return false; }
 
+static inline bool asi_in_critical_section(void) { return false; }
+
 static inline void asi_exit(void) { }
 
 static inline bool asi_is_restricted(void) { return false; }
@@ -64,6 +66,14 @@ static inline struct asi *asi_get_target(struct task_struct *p) { return NULL; }
 static inline pgd_t *asi_pgd(struct asi *asi) { return NULL; }
 
 static inline void asi_handle_switch_mm(void) { }
+
+static inline void asi_init_thread_state(struct thread_struct *thread) { }
+
+static inline void asi_intr_enter(void) { }
+
+static inline int asi_intr_nest_depth(void) { return 0; }
+
+static inline void asi_intr_exit(void) { }
 
 #define static_asi_enabled() false
 
