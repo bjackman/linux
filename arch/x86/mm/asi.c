f5554a23cc8f621b8d533838925169d94cb8bb9a
Brendan Jackman <jackmanb@google.com>
2023-08-22 18:16:54 +0000

    mm: asi: Add infrastructure for boot-time enablement
    Add a boot time parameter to control the newly added X86_FEATURE_ASI.
    "asi=on" or "asi=off" can be used in the kernel command line to enable
    or disable ASI at boot time. If not specified, ASI enablement depends
    on CONFIG_ADDRESS_SPACE_ISOLATION_DEFAULT_ON, which is off by default.
    asi_check_boottime_disable() is modeled after
    pti_check_boottime_disable().
    
    The boot parameter is currently ignored until ASI is fully functional.
    
    Once we have a set of ASI features checked in that we have actually
    tested, we will stop ignoring the flag. But for now let's just add the
    infrastructure so we can implement the usage code.
    
    Ignoring checkpatch.pl CONFIG_DESCRIPTION because the _DEFAULT_ON
    Kconfig is trivial to explain.
    
    Checkpatch-args: --ignore CONFIG_DESCRIPTION
    Co-developed-by: Junaid Shahid <junaids@google.com>
    Signed-off-by: Junaid Shahid <junaids@google.com>
    Co-developed-by: Yosry Ahmed <yosryahmed@google.com>
    Signed-off-by: Yosry Ahmed <yosryahmed@google.com>
    Signed-off-by: Brendan Jackman <jackmanb@google.com>
    Change-Id: I04f6329970b76cc9abbbf28ff92cbe8bba42d4be

--- arch/x86/mm/asi.c
+++ arch/x86/mm/asi.c
@@ -38,6 +38,9 @@ static inline bool asi_class_id_valid(enum asi_class_id class_id)
 
 static inline bool asi_class_initialized(enum asi_class_id class_id)
 {
+	if (!boot_cpu_has(X86_FEATURE_ASI))
+		return 0;
+
 	if (WARN_ON(!asi_class_id_valid(class_id)))
 		return false;
 
@@ -60,6 +63,9 @@ EXPORT_SYMBOL_GPL(asi_init_class);
 
 void asi_uninit_class(enum asi_class_id class_id)
 {
+	if (!boot_cpu_has(X86_FEATURE_ASI))
+		return;
+
 	if (!asi_class_initialized(class_id))
 		return;
 
@@ -75,10 +81,35 @@ const char *asi_class_name(enum asi_class_id class_id)
 	return asi_class_names[class_id];
 }
 
-static void __asi_destroy(struct asi *asi)
+void __init asi_check_boottime_disable(void)
 {
-	lockdep_assert_held(&asi->mm->asi_init_lock);
+	bool enabled = IS_ENABLED(CONFIG_MITIGATION_ADDRESS_SPACE_ISOLATION_DEFAULT_ON);
+	char arg[4];
+	int ret;
 
+	ret = cmdline_find_option(boot_command_line, "asi", arg, sizeof(arg));
+	if (ret == 3 && !strncmp(arg, "off", 3)) {
+		enabled = false;
+		pr_info("ASI disabled through kernel command line.\n");
+	} else if (ret == 2 && !strncmp(arg, "on", 2)) {
+		enabled = true;
+		pr_info("Ignoring asi=on param while ASI implementation is incomplete.\n");
+	} else {
+		pr_info("ASI %s by default.\n",
+			enabled ? "enabled" : "disabled");
+	}
+
+	if (enabled)
+		pr_info("ASI enablement ignored due to incomplete implementation.\n");
+}
+
+static inline void __asi_destroy(struct asi *asi)
+{
+	WARN_ON_ONCE(asi->ref_count <= 0);
+	if (--(asi->ref_count) == 0) {
+		free_pages((ulong)asi->pgd, pgd_allocation_order());
+		memset(asi, 0, sizeof(struct asi));
+	}
 }
 
 int asi_init(struct mm_struct *mm, enum asi_class_id class_id, struct asi **out_asi)
@@ -87,6 +118,9 @@ int asi_init(struct mm_struct *mm, enum asi_class_id class_id, struct asi **out_
 	int err = 0;
 
 	*out_asi = NULL;
+
+	if (!boot_cpu_has(X86_FEATURE_ASI))
+		return 0;
 
 	if (WARN_ON(!asi_class_initialized(class_id)))
 		return -EINVAL;
@@ -134,7 +168,7 @@ void asi_destroy(struct asi *asi)
 {
 	struct mm_struct *mm;
 
-	if (!asi)
+	if (!boot_cpu_has(X86_FEATURE_ASI) || !asi)
 		return;
 
 	if (WARN_ON(!asi_class_initialized(asi->class_id)))
@@ -146,11 +180,7 @@ void asi_destroy(struct asi *asi)
 	 * to block concurrent asi_init calls.
 	 */
 	mutex_lock(&mm->asi_init_lock);
-	WARN_ON_ONCE(asi->ref_count <= 0);
-	if (--(asi->ref_count) == 0) {
-		free_pages((ulong)asi->pgd, pgd_allocation_order());
-		memset(asi, 0, sizeof(struct asi));
-	}
+	__asi_destroy(asi);
 	mutex_unlock(&mm->asi_init_lock);
 }
 EXPORT_SYMBOL_GPL(asi_destroy);
@@ -267,6 +297,9 @@ static noinstr void __asi_enter(void)
 
 noinstr void asi_enter(struct asi *asi)
 {
+	if (!cpu_feature_enabled(X86_FEATURE_ASI))
+		return;
+
 	VM_WARN_ON_ONCE(!asi);
 
 	/* Should not have an asi_enter() without a prior asi_relax(). */
@@ -281,8 +314,10 @@ EXPORT_SYMBOL_GPL(asi_enter);
 
 noinstr void asi_relax(void)
 {
-	barrier();
-	asi_set_target(current, NULL);
+	if (cpu_feature_enabled(X86_FEATURE_ASI)) {
+		barrier();
+		asi_set_target(current, NULL);
+	}
 }
 EXPORT_SYMBOL_GPL(asi_relax);
 
@@ -290,6 +325,9 @@ noinstr void asi_exit(void)
 {
 	u64 unrestricted_cr3;
 	struct asi *asi;
+
+	if (!cpu_feature_enabled(X86_FEATURE_ASI))
+		return;
 
 	preempt_disable_notrace();
 
@@ -322,6 +360,9 @@ EXPORT_SYMBOL_GPL(asi_exit);
 
 void asi_init_mm_state(struct mm_struct *mm)
 {
+	if (!boot_cpu_has(X86_FEATURE_ASI))
+		return;
+
 	memset(mm->asi, 0, sizeof(mm->asi));
 	mutex_init(&mm->asi_init_lock);
 }
