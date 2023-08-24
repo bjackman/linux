20f52c790d750b20f269a3bb200658dca713dcf7
Brendan Jackman <jackmanb@google.com>
2023-08-24 21:37:19 +0000

    mm: asi: Avoid warning from NMI userspace accesses in ASI context
    nmi_uaccess_okay() emits a warning if current CR3 != mm->pgd.
    Limit the warning to only when ASI is not active.
    
    Co-developed-by: Junaid Shahid <junaids@google.com>
    Signed-off-by: Junaid Shahid <junaids@google.com>
    Co-developed-by: Yosry Ahmed <yosryahmed@google.com>
    Signed-off-by: Yosry Ahmed <yosryahmed@google.com>
    Signed-off-by: Brendan Jackman <jackmanb@google.com>
    Change-Id: I7ab25173b966e3469d79c0a74c753a2d07c4a4df

--- arch/x86/mm/tlb.c
+++ arch/x86/mm/tlb.c
@@ -1808,6 +1808,22 @@ void arch_tlbbatch_flush(struct arch_tlbflush_unmap_batch *batch)
 	put_cpu();
 }
 
+static inline bool cr3_matches_current_mm(void)
+{
+	struct asi *asi = asi_get_current();
+	unsigned long pgd_asi = __pa(asi_pgd(asi));
+	unsigned long pgd_cr3;
+
+	/*
+	 * Prevent read_cr3_pa -> [NMI, asi_exit] -> asi_get_current,
+	 * otherwise we might find CR3 pointing to the ASI PGD but not
+	 * find a current ASI domain.
+	 */
+	barrier();
+	pgd_cr3 = read_cr3_pa();
+	return pgd_cr3 == __pa(current->mm->pgd) || pgd_cr3 == pgd_asi;
+}
+
 /*
  * Blindly accessing user memory from NMI context can be dangerous
  * if we're in the middle of switching the current user task or
@@ -1823,10 +1839,10 @@ bool nmi_uaccess_okay(void)
 	VM_WARN_ON_ONCE(!loaded_mm);
 
 	/*
-	 * The condition we want to check is
-	 * current_mm->pgd == __va(read_cr3_pa()).  This may be slow, though,
-	 * if we're running in a VM with shadow paging, and nmi_uaccess_okay()
-	 * is supposed to be reasonably fast.
+	 * The condition we want to check that CR3 points to either
+	 * current_mm->pgd or an appropriate ASI PGD. Reading CR3 may be slow,
+	 * though, if we're running in a VM with shadow paging, and
+	 * nmi_uaccess_okay() is supposed to be reasonably fast.
 	 *
 	 * Instead, we check the almost equivalent but somewhat conservative
 	 * condition below, and we rely on the fact that switch_mm_irqs_off()
@@ -1835,7 +1851,7 @@ bool nmi_uaccess_okay(void)
 	if (loaded_mm != current_mm)
 		return false;
 
-	VM_WARN_ON_ONCE(__pa(current_mm->pgd) != read_cr3_pa());
+	VM_WARN_ON_ONCE(!cr3_matches_current_mm());
 
 	return true;
 }
