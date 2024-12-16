f8a2a094ee07b06926d9ef1cf2d2ac6fedc58769
Brendan Jackman <jackmanb@google.com>
2024-12-16 16:10:45 +0000

    mm: asi: Introduce ASI core API
    Introduce core API for Address Space Isolation (ASI).  Kernel address
    space isolation provides the ability to run some kernel
    code with a restricted kernel address space.
    
    There can be multiple classes of such restricted kernel address spaces
    (e.g. KPTI, KVM-PTI etc.). Each ASI class is identified by an index.
    The ASI class can register some hooks to be called when
    entering/exiting the restricted address space.
    
    Currently, there is a fixed maximum number of ASI classes supported.
    In addition, each process can have at most one restricted address space
    from each ASI class. Neither of these are inherent limitations and
    are merely simplifying assumptions for the time being.
    
    To keep things simpler for the time being, we disallow context switches
    within the restricted address space. In the future, we would be able to
    relax this limitation for the case of context switches to different
    threads within the same process (or to the idle thread and back).
    
    Note that this doesn't really support protecting sibling VM guests
    within the same VMM process from one another. From first principles
    it seems unlikely that anyone who cares about VM isolation would do
    that, but there could be a use-case to think about. In that case need
    something like the OTHER_MM logic might be needed, but specific to
    intra-process guest separation.
    
    [0]:
    https://lore.kernel.org/kvm/1562855138-19507-1-git-send-email-alexandre.chartre@oracle.com
    
    Notes about RFC-quality implementation details:
    
     - Ignoring checkpatch.pl AVOID_BUG.
     - The dynamic registration of classes might be pointless complexity.
       This was kept from RFCv1 without much thought.
     - The other-mm logic is also perhaps overly complex, suggestions are
       welcome for how best to tackle this (or we could just forget about
       it for the moment, and rely on asi_exit() happening in process
       switch).
     - The taint flag definitions would probably be clearer with an enum or
       something.
    
    TODO: Address feedback in https://lore.kernel.org/linux-mm/20250219105503.GKZ7W4h6QW1CNj48U9@fat_crate.local/
    
    Co-developed-by: Ofir Weisse <oweisse@google.com>
    Signed-off-by: Ofir Weisse <oweisse@google.com>
    Co-developed-by: Junaid Shahid <junaids@google.com>
    Signed-off-by: Junaid Shahid <junaids@google.com>
    Signed-off-by: Brendan Jackman <jackmanb@google.com>
    Change-Id: I886c39891e43f240b9981dff3a89f863c4d71c03

--- arch/x86/include/asm/asi.h
+++ arch/x86/include/asm/asi.h
@@ -2,6 +2,8 @@
 #ifndef _ASM_X86_ASI_H
 #define _ASM_X86_ASI_H
 
+#include <linux/sched.h>
+
 #include <asm/cpufeature.h>
 
 void asi_check_boottime_disable(void);
@@ -12,8 +14,171 @@ static inline bool asi_enabled_static(void)
 	return cpu_feature_enabled(X86_FEATURE_ASI);
 }
 
+/*
+ * Overview of API usage by ASI clients:
+ *
+ * Setup: First call asi_init_domain() to create a domain. At present only one
+ * domain can be created per mm per class, but it's safe to asi_init_domain()
+ * this domain multiple times. For each asi_init_domain() call you must call
+ * asi_destroy() AFTER you are certain all CPUs have exited the nonsensitive
+ * address space (by calling asi_exit()).
+ *
+ * Runtime usage:
+ *
+ * 1. Call asi_enter() to switch to the nonsensitive address space. This can't be
+ *    from an interrupt or exception handler and preemption must be disabled.
+ *
+ * 2. Execute untrusted code.
+ *
+ * 3. Call asi_relax() to inform the ASI subsystem that untrusted code execution
+ *    is finished. This doesn't cause any address space change. This can't be
+ *    from an interrupt or exception handler and preemption must be disabled.
+ *
+ * 4. Either:
+ *
+ *    a. Go back to 1.
+ *
+ *    b. Call asi_exit() before returning to userspace. This immediately
+ *       switches to the sensitive address space.
+ *
+ * The region between 1 and 3 is called the "ASI critical section". During the
+ * critical section, it is a bug to access any sensitive data, and you mustn't
+ * sleep.
+ *
+ * The restriction on sleeping is not really a fundamental property of ASI.
+ * However for performance reasons it's important that the critical section is
+ * absolutely as short as possible. So the ability to do sleepy things like
+ * taking mutexes oughtn't to confer any convenience on API users.
+ *
+ * Similarly to the issue of sleeping, the need to asi_exit in case 4b is not a
+ * fundamental property of the system but a limitation of the current
+ * implementation. With further work it is possible to context switch
+ * from and/or to the nonsensitive address space, and to return to userspace
+ * directly from the nonsensitive address space, or _in_ it.
+ *
+ * Note that the critical section only refers to the direct execution path from
+ * asi_enter to asi_relax: it's fine to access sensitive data from exceptions
+ * and interrupt handlers that occur during that time. ASI will re-enter the
+ * nonsensitive address space before returning from the outermost
+ * exception/interrupt.
+ *
+ * Note: ASI does not modify KPTI behaviour; when ASI and KPTI run together
+ * there are 2+N address spaces per task: the sensitive kernel address space,
+ * the user address space, and one nonsensitive (kernel) address space for each of
+ * the N ASI classes.
+ */
+
+typedef u8 asi_taints_t;
+
+/*
+ * ASI uses a per-CPU tainting model to track what mitigation actions are
+ * required on domain transitions. Taints exist along two dimensions:
+ *
+ *  - Who touched the CPU (guest, unprotected kernel, userspace).
+ *
+ *  - What kind of state might remain: "data" means there might be data owned by
+ *    a victim domain left behind in a sidechannel. "Control" means there might
+ *    be state controlled by an attacker domain left behind in the branch
+ *    predictor.
+ *
+ *    In principle the same domain can be both attacker and victim, thus we have
+ *    both data and control taints for userspace, although there's no point in
+ *    trying to protect against attacks from the kernel itself, so there's no
+ *    ASI_TAINT_KERNEL_CONTROL.
+ */
+#define ASI_TAINT_KERNEL_DATA		((asi_taints_t)BIT(0))
+#define ASI_TAINT_USER_DATA		((asi_taints_t)BIT(1))
+#define ASI_TAINT_GUEST_DATA		((asi_taints_t)BIT(2))
+#define ASI_TAINT_OTHER_MM_DATA		((asi_taints_t)BIT(3))
+#define ASI_TAINT_USER_CONTROL		((asi_taints_t)BIT(4))
+#define ASI_TAINT_GUEST_CONTROL		((asi_taints_t)BIT(5))
+#define ASI_TAINT_OTHER_MM_CONTROL	((asi_taints_t)BIT(6))
+#define ASI_NUM_TAINTS			6
+static_assert(BITS_PER_BYTE * sizeof(asi_taints_t) >= ASI_NUM_TAINTS);
+
+#define ASI_TAINTS_CONTROL_MASK \
+	(ASI_TAINT_USER_CONTROL | ASI_TAINT_GUEST_CONTROL | ASI_TAINT_OTHER_MM_CONTROL)
+
+#define ASI_TAINTS_DATA_MASK \
+	(ASI_TAINT_KERNEL_DATA | ASI_TAINT_USER_DATA | ASI_TAINT_OTHER_MM_DATA)
+
+#define ASI_TAINTS_GUEST_MASK (ASI_TAINT_GUEST_DATA | ASI_TAINT_GUEST_CONTROL)
+#define ASI_TAINTS_USER_MASK (ASI_TAINT_USER_DATA | ASI_TAINT_USER_CONTROL)
+
+/* The taint policy tells ASI how a class interacts with the CPU taints */
+struct asi_taint_policy {
+	/*
+	 * What taints would necessitate a flush when entering the domain, to
+	 * protect it from attack by prior domains?
+	 */
+	asi_taints_t prevent_control;
+	/*
+	 * What taints would necessetate a flush when entering the domain, to
+	 * protect former domains from attack by this domain?
+	 */
+	asi_taints_t protect_data;
+	/* What taints should be set when entering the domain? */
+	asi_taints_t set;
+};
+
+/*
+ * An ASI domain (struct asi) represents a nonsensitive address space. The
+ * sensitive address space (and user address space under PTI) are not
+ * represented as a domain.
+ */
+struct asi {
+	pgd_t *pgd;
+	struct mm_struct *mm;
+	int64_t ref_count;
+	enum asi_class_id class_id;
+};
 void asi_init(void);
 
+DECLARE_PER_CPU_ALIGNED(struct asi *, curr_asi);
+
+void asi_init_mm_state(struct mm_struct *mm);
+
+int asi_init_class(enum asi_class_id class_id, struct asi_taint_policy *taint_policy);
+void asi_uninit_class(enum asi_class_id class_id);
+const char *asi_class_name(enum asi_class_id class_id);
+
+int asi_init_domain(struct mm_struct *mm, enum asi_class_id class_id, struct asi **out_asi);
+void asi_destroy(struct asi *asi);
+
+/* Enter an ASI domain (nonsensitive address space) and begin the critical section. */
+void asi_enter(struct asi *asi);
+
+/*
+ * Leave the "tense" state if we are in it, i.e. end the critical section. We
+ * will stay relaxed until the next asi_enter.
+ */
+void asi_relax(void);
+
+/* Immediately exit the nonsensitive address space if in it */
+void asi_exit(void);
+
+/* The target is the domain we'll enter when returning to process context. */
+static __always_inline struct asi *asi_get_target(struct task_struct *p)
+{
+	return p->thread.asi_state.target;
+}
+
+static __always_inline void asi_set_target(struct task_struct *p,
+					   struct asi *target)
+{
+	p->thread.asi_state.target = target;
+}
+
+#define INIT_MM_ASI(init_mm) \
+	.asi_init_lock = __MUTEX_INITIALIZER(init_mm.asi_init_lock),
+
+void asi_handle_switch_mm(void);
+
+extern struct asi __asi_global_nonsensitive;
+#define ASI_GLOBAL_NONSENSITIVE (&__asi_global_nonsensitive)
+
+void asi_map(struct page *page, int numpages);
+void asi_unmap(struct page *page, int numpages);
 extern pgd_t *asi_nonsensitive_pgd;
 
 #endif /* _ASM_X86_ASI_H */
