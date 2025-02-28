5a878b0f7f5dff52fdf0f95bf66202e1141be558
Brendan Jackman <jackmanb@google.com>
2025-02-28 16:24:33 +0000

    github: Add update_branches.sh, with akpm-mm

---
+++ .github/scripts/update_branches.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eux
+
+if ! git remote | grep '^akpm-mm$'; then
+    git remote add akpm-mm https://git.kernel.org/pub/scm/linux/kernel/git/akpm/mm.git
+fi
+
+if ! git remote | grep '^linus$'; then
+    git remote add linus https://github.com/torvalds/linux.git
+fi
+
+for upstream_branch in linus/master akpm-mm/mm-stable; do
+    git checkout github/$upstream_branch
+    git merge --no-edit $upstream_branch
+    git merge --no-edit github-base
+    git push origin github/$upstream_branch:github/$upstream_branch
+done
