dcedcb3a85bcc83a22a787a4102e1514b2dc8a5d
Brendan Jackman <bhenryj0117@gmail.com>
2025-03-01 13:32:30 +0100

    github: Add akpm's mm-unstable tree, with rebasing

--- .github/scripts/update_branches.sh
+++ .github/scripts/update_branches.sh
@@ -2,12 +2,16 @@
 
 set -eux
 
+# Map remote names to remote URLs.
 declare -A REMOTES
 REMOTES=(
     ["akpm-mm"]="https://git.kernel.org/pub/scm/linux/kernel/git/akpm/mm.git"
     ["linus"]="https://github.com/torvalds/linux.git"
 )
 
+# Create remotes if they don't exist.
+# (They will never exist when running on Github, but handling pre-existing
+# branches makes this script runnable locally for testing).
 for remote in "${!REMOTES[@]}"; do
     if ! git remote | grep "^${remote}$"; then
         git remote add "$remote" "${REMOTES["$remote"]}"
@@ -16,20 +20,50 @@ done
 
 git fetch --all
 
+# Map remote branches to update strategies.
+#
+# "merge" means we just repeatedly merge the branch and github-base into the
+# shadow branch. This is cleaner for branches that actually feed into Linus'
+# tree.
+#
+# "rebase" means the shadow branch is just the upstream branch with a single
+# commit on top of it that merges in github-base. This avoids merge conflicts
+# when testing upstream branches that get rebased.
 declare -A BRANCHES
 BRANCHES=(
     ["linus/master"]=merge
     ["akpm-mm/mm-stable"]=merge
+    ["akpm-mm/mm-unstable"]=rebase
 )
 
 for upstream_branch in "${!BRANCHES[@]}"; do
-    git checkout "github/$upstream_branch"
-    if [[ "${BRANCHES["$upstream_branch"]}" = merge ]]; then
-        git merge --no-edit "$upstream_branch"
+    # If the branch exists, check it out, otherwise create it
+    if git branch | grep -q "$upstream_branch"; then
+        git checkout "github/$upstream_branch"
     else
-        echo "Invalid branch setting '${BRANCHES["$upstream_branch"]}' for $upstream_branch, must be 'merge'"
+        git checkout -b "github/$upstream_branch" "$upstream_branch"
+    fi
+
+    # Update the shadow branch with the latest upstream contents, then merge in
+    # github-base.
+    mode=${BRANCHES["$upstream_branch"]}
+    if [[ "$mode" = merge ]]; then
+        git merge --no-edit "$upstream_branch"
+        git merge --no-edit github-base
+    elif [[ "$mode" = rebase ]]; then
+        # This is called "rebase" because even though we don't invoke "git
+        # rebase", we're "rebasing the merge commit of github-base onto the
+        # shadow branch each time. (It's not actually equivalent to doing that,
+        # because we also amend the merge commit to merge in the latest version
+        # of github-base, instead of just having a pointless series of repeated
+        # merge commits).
+        git reset --hard "$upstream_branch"
+        git merge --no-edit github-base --allow-unrelated-histories
+    else
+        echo "Invalid branch setting '$mode' for $upstream_branch, must be 'merge' or 'rebase'"
         exit 1
     fi
-    git merge --no-edit github-base
+
+    # Now merge in github-base and we're done.
     git push origin "github/$upstream_branch:github/$upstream_branch"
 done
