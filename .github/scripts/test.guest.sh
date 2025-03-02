c57591b3be1ebf4db9d31aaec9035d266ca48e06
Brendan Jackman <bhenryj0117@gmail.com>
2025-03-02 15:37:45 +0100

    github: Add userfaultfd tests

--- .github/scripts/test.guest.sh
+++ .github/scripts/test.guest.sh
@@ -9,4 +9,4 @@ cd mm/
 
 # These are the only tests I've managed to get working so far.
 # See https://lore.kernel.org/linux-mm/20250228-mm-selftests-v3-0-958e3b6f0203@google.com/
-./run_vmtests.sh -t "mmap gup_test compaction migration page_frag"
+./run_vmtests.sh -t "mmap gup_test compaction migration page_frag userfaultfd"
