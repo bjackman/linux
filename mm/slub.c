a900d6559309f6ab462242b813dee326e56f80f1
Brendan Jackman <jackmanb@google.com>
2025-03-12 16:04:49 +0000

    [RFC HACKS] mm/slub: Set __GFP_SENSITIVE for reclaimable slabs
    It's not currently possible allocate reclaimable, nonsensitive pages.
    For the moment, just add __GFP_SENSITIVE.
    
    This will need to be fixed before this can be a [PATCH].
    
    Signed-off-by: Brendan Jackman <jackmanb@google.com>

--- mm/slub.c
+++ mm/slub.c
@@ -5808,7 +5808,11 @@ static int calculate_sizes(struct kmem_cache_args *args, struct kmem_cache *s)
 		s->allocflags |= GFP_DMA32;
 
 	if (s->flags & SLAB_RECLAIM_ACCOUNT)
-		s->allocflags |= __GFP_RECLAIMABLE;
+		/*
+		 * TODO: Cannot currently allocate reclaimable, nonsensitive
+		 * pages. For the moment, just add __GFP_SENSITIVE.
+		 */
+		s->allocflags |= __GFP_RECLAIMABLE | __GFP_SENSITIVE;
 
 	/*
 	 * Determine the number of objects per slab
