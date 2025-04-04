4ad49d6ef83c24d12390afd72cc000f7a73ba6c4
Nikita Kalyazin <kalyazin@amazon.com>
2025-04-04 14:18:03 +0000

    mm: userfaultfd: add UFFD_FEATURE_MINOR_GUEST_MEMFD
    Signed-off-by: Nikita Kalyazin <kalyazin@amazon.com>

--- fs/userfaultfd.c
+++ fs/userfaultfd.c
@@ -1998,7 +1998,8 @@ static int userfaultfd_api(struct userfaultfd_ctx *ctx,
 	uffdio_api.features = UFFD_API_FEATURES;
 #ifndef CONFIG_HAVE_ARCH_USERFAULTFD_MINOR
 	uffdio_api.features &=
-		~(UFFD_FEATURE_MINOR_HUGETLBFS | UFFD_FEATURE_MINOR_SHMEM);
+		~(UFFD_FEATURE_MINOR_HUGETLBFS | UFFD_FEATURE_MINOR_SHMEM |
+		UFFD_FEATURE_MINOR_GUEST_MEMFD);
 #endif
 	if (!pgtable_supports_uffd_wp())
 		uffdio_api.features &= ~UFFD_FEATURE_PAGEFAULT_FLAG_WP;
