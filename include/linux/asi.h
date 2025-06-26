690c80b057cf69760929fb5c906432a7d91770e5
Brendan Jackman <jackmanb@google.com>
2025-06-26 09:51:18 +0000

    HACK: Dumb header dependency workaround (asi_exit.h)

--- include/linux/asi.h
+++ include/linux/asi.h
@@ -2,6 +2,7 @@
 #ifndef _INCLUDE_ASI_H
 #define _INCLUDE_ASI_H
 
+#include <linux/asi_exit.h>
 #include <linux/types.h>
 
 enum asi_class_id {
@@ -41,7 +42,7 @@ static inline void asi_destroy(struct asi *asi) { }
 static inline void asi_init(void) { };
 
 static inline void asi_enter(struct asi *asi) { }
-static inline void asi_exit(void) { }
+/* asi_exit() defined via yucky asi_exit.h. */
 static inline void asi_relax(void) { }
 
 static inline struct asi *asi_get_current(void) { return NULL; }
