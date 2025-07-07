72e66a5b82d9cb9d078478cee018b4c27cfe4985
Brendan Jackman <jackmanb@google.com>
2025-07-07 16:00:54 +0000

    x86: mm: introduce the mermap
    The mermap provides a fast way to create ephemeral mm-local mappings of
    physical pages. The purpose of this is to access pages that have been
    removed from the direct map. Potential use cases are:
    
    1. For zeroing __GFP_UNMAPPED pages (added in a later patch).
    
    2. For populating guest_memfd pages that are protected by the
       GUEST_MEMFD_NO_DIRECT_MAP feature [0].
    
    3. For efficient access of pages protected by Address Space Isolation
       [1].
    
    [0] https://lore.kernel.org/all/20250924151101.2225820-1-patrick.roy@campus.lmu.de/
    [1] https://linuxasi.dev
    
    The details of this mechanism are described in the API comments. However
    the key idea is to use CPU-local virtual regions to avoid a need for
    synchronizing. On x86, this can also be used to prevent TLB shootdowns.
    
    Because the virtual region is CPU-local, allocating from the mermap
    disables migration. The caller is forbidden to use the returned value
    from any other context, and migration is re-enabled when it's freed.
    
    One might notice that mermap_get() bears a strong similarity to
    kmap_local_page(). The most important differences between mermap_get()
    and kmap_local_page() are:
    
    1. mermap_get() allows mapping variable sizes while kmap_local_page()
       specifically maps a single order-0 page.
    2. As a consequence of 1 (combined with the need for mermap_get() to be
       an extremely simple allocator), mermap_get() should be expected to
       fail, while kmap_local_page() is guaranteed to work up to a certain
       degree of nesting.
    3. While the mappings provided by kmap_local_page() are _logically_
       local to the calling context (it's a bug for software to access them
       from elsewhere), they are _physically_ installed into the shared
       kernel pagetables. This means their locality doesn't provide any
       protection from hardware attacks. In contrast, the mermap is
       physically local to the creating mm, taking advantage of the new
       mm-local kernel address region.
    
    So that the mermap is available even in contexts where failure is not
    tolerable there is also a _reserved() variant, which is fixed at
    allocating a single base page. This is useful, for example, for zeroing
    __GFP_UNMAPPED pages, where handling failure would be extremely
    inconvenient. The _reserved() variant is simply implemented by leaving
    one base-page space unavailable for non-_reserved allocations, and
    requiring an atomic context.
    
    This mechanism obviously requires manipulating pagetables. The kernel
    doesn't have a "library" that is 100% suitable for the mermap's needs
    here. This is resolved with a hack, namely exploiting
    apply_to[_existing_page_range(), which is _almost_ suitable for the
    requirements. This will need some later refactoring (perhaps creating
    the "library") to resolve the hacks it introduces, which are:
    
    1. It introduces an indirect branch, which is likely to be pretty slow
       on some platforms.
    
    2. It uses a magic sentinel pagetable value, instead of pte_none(), for
       unmapped regions, to trick apply_to_existing_page_range() into
       operating on them (while still ensuring no pagetable allocations take
       place).
    
    Signed-off-by: Brendan Jackman <jackmanb@google.com>
    Change-Id: I9cf0f364403fe9da6b547dbf61a06a4e0696cbbe

--- kernel/fork.c
+++ kernel/fork.c
@@ -13,6 +13,7 @@
  */
 
 #include <linux/anon_inodes.h>
+#include <linux/mermap.h>
 #include <linux/slab.h>
 #include <linux/sched/autogroup.h>
 #include <linux/sched/mm.h>
@@ -1129,6 +1130,7 @@ static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p,
 
 	mm->user_ns = get_user_ns(user_ns);
 	lru_gen_init_mm(mm);
+
 	return mm;
 
 fail_pcpu:
@@ -1172,6 +1174,7 @@ static inline void __mmput(struct mm_struct *mm)
 	ksm_exit(mm);
 	khugepaged_exit(mm); /* must run before exit_mmap */
 	exit_mmap(mm);
+	mermap_mm_teardown(mm);
 	mm_put_huge_zero_folio(mm);
 	set_mm_exe_file(mm, NULL);
 	if (!list_empty(&mm->mmlist)) {
