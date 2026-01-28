bc764b2a54bbbaa15620beb09e6dcbdbdcac67e0
Brendan Jackman <jackmanb@google.com>
2026-01-28 15:31:01 +0000

    misc build fixes
    Change-Id: Ic43509ebe45884005d7ed4f88a3b4f56870919c4

--- arch/x86/include/asm/set_memory.h
+++ arch/x86/include/asm/set_memory.h
@@ -88,6 +88,7 @@ int set_pages_rw(struct page *page, int numpages);
 int set_direct_map_invalid_noflush(struct page *page);
 int set_direct_map_default_noflush(struct page *page);
 int set_direct_map_valid_noflush(struct page *page, unsigned nr, bool valid);
+struct folio;
 int folio_zap_direct_map(struct folio *folio);
 int folio_restore_direct_map(struct folio *folio);
 bool kernel_page_present(struct page *page);
