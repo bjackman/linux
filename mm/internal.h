2388afba4980ba1551f550dbf331b2860fc1105e
Brendan Jackman <jackmanb@google.com>
2026-03-16 12:52:38 +0000

    mm: Add more flags for __apply_to_page_range()
    Add two flags to make this API more generic:
    
    1. Separate "create" into two levels - one to allow creating new
       mappings without allocating pagetables, and one for the current
       behaviour that allows both of these.
    
    2. Create a new flag to report that the caller has taken care of
       synchronization and no locks are required.
    
    Both of these will serve to allow calling this API from restricted
    contexts where allocation and pagetable locking are not possible.
    
    Signed-off-by: Brendan Jackman <jackmanb@google.com>

--- mm/internal.h
+++ mm/internal.h
@@ -1953,9 +1953,30 @@ bool may_expand_vm(struct mm_struct *mm, const vma_flags_t *vma_flags,
 
 /*
  * Create a mapping if it doesn't exist. (Otherwise, skip regions with no
- * existing mapping, and return an error for regions with no leaf pagetable).
+ * existing mapping). This doesn't allow allocating, most users will want
+ * PGRANGE_ALLOC.
+ *
+ * Do not test this bit directly as it is implied by PGRANGE_ALLOC, use
+ * pgrange_create() instead.
  */
 #define PGRANGE_CREATE		(1 << 0)
+/*
+ * Allocate a pagetable if one is missing. (Otherwise, return an error for
+ * regions with no leaf pagetable). Also implies PGRANGE_CREATE.
+ */
+#define PGRANGE_ALLOC		(1 << 1)
+/*
+ * Do not take any locks. This means the caller has taken care of
+ * synchronisation. This is incompatible with PGRANGE_ALLOC and also with
+ * mm=&init_mm.
+ */
+#define PGRANGE_NOLOCK		(1 << 2)
+
+
+static inline bool pgrange_create(unsigned int flags)
+{
+	return flags & (PGRANGE_CREATE | PGRANGE_ALLOC);
+}
 
 int __apply_to_page_range(struct mm_struct *mm, unsigned long addr,
 			  unsigned long size, pte_fn_t fn,
